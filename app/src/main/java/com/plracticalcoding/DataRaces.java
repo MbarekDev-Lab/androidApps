b42cb121660a5b2bf8d39e93e4404ee9dfc00018
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-03-16 19:50:26 +0100

    Data Races

---
+++ app/src/main/java/com/plracticalcoding/DataRaces.java
@@ -0,0 +1,38 @@
+package com.plracticalcoding;
+
+public class DataRaces {
+    public static void main(String[] args) {
+        InnerSharedClass sharedClass = new InnerSharedClass();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.checkForDataRace();
+            }
+
+        });
+        thread1.start();
+        thread2.start();
+    }
+
+    public static class InnerSharedClass {
+        private int x = 0;
+        private int y = 0;
+
+        public void increment() {
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace() {
+            if (y > x) {
+                System.out.println("y > x - Data Race is detected");
+            }
+        }
+    }
+}
