db5833cac8ab6dbeacb0da1616eb78ed16270a04
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-11-04 08:58:43 +0100

    Implemented math game logic

--- app/src/main/java/com/plracticalcoding/quizGame/mathGame/GameActivity.java
+++ app/src/main/java/com/plracticalcoding/quizGame/mathGame/GameActivity.java
@@ -1,6 +1,8 @@
 package com.plracticalcoding.quizGame.mathGame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 import com.plracticalcoding.myapplication.R;
 
 public class GameActivity extends AppCompatActivity {
+    Button addition, subtraction, multiplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        addition = findViewById(R.id.addition);
+        subtraction = findViewById(R.id.subtraction);
+        multiplication = findViewById(R.id.multiplication);
+
+
+        addition.setOnClickListener(v -> {
+            startActivity(new Intent(GameActivity.this, MathGame.class));
         });
+
     }
-
-
-
-
-
-
-
 
 
 }
--- app/src/main/java/com/plracticalcoding/quizGame/mathGame/MathGame.java
+++ app/src/main/java/com/plracticalcoding/quizGame/mathGame/MathGame.java
@@ -1,30 +1,110 @@
 package com.plracticalcoding.quizGame.mathGame;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import com.plracticalcoding.myapplication.R;
 
+import java.util.Random;
+
 public class MathGame extends AppCompatActivity {
+    TextView scoreText, lifeText, timeText, questionText;
+    EditText answerInput;
+    Button okButton, nextButton;
+
+    int score = 0;
+    int life = 3;
+    int timeValue = 60;
+    int correctAnswer;
+    CountDownTimer timer;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_math_game);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        scoreText = findViewById(R.id.textView5);
+        lifeText = findViewById(R.id.textView7);
+        timeText = findViewById(R.id.textView9);
+        questionText = findViewById(R.id.textView13);
+        answerInput = findViewById(R.id.editTextText2);
+        okButton = findViewById(R.id.button10);
+        nextButton = findViewById(R.id.button11);
+
+        random = new Random();
+        gameContinue();
+
+        okButton.setOnClickListener(view -> checkAnswer());
+        nextButton.setOnClickListener(view -> gameContinue());
     }
 
+    private void gameContinue() {
+        int num1 = random.nextInt(50);
+        int num2 = random.nextInt(50);
+        correctAnswer = num1 + num2;
 
+        questionText.setText(num1 + " + " + num2 + " = ?");
+        answerInput.setText("");
 
+        startTimer();
+    }
 
+    private void startTimer() {
+        if (timer != null) timer.cancel();
+
+        timer = new CountDownTimer(timeValue * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeValue = (int) (millisUntilFinished / 1000);
+                timeText.setText(String.valueOf(timeValue));
+            }
+
+            @Override
+            public void onFinish() {
+                life--;
+                lifeText.setText(String.valueOf(life));
+                if (life == 0) {
+                    Toast.makeText(MathGame.this, "Game Over!", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(MathGame.this, "Time’s up!", Toast.LENGTH_SHORT).show();
+                    gameContinue();
+                }
+            }
+        }.start();
+    }
+
+    private void checkAnswer() {
+        String answerString = answerInput.getText().toString();
+        if (answerString.equals("")) {
+            Toast.makeText(this, "Please enter an answer", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int playerAnswer = Integer.parseInt(answerString);
+        if (playerAnswer == correctAnswer) {
+            score++;
+            scoreText.setText(String.valueOf(score));
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+            gameContinue();
+        } else {
+            life--;
+            lifeText.setText(String.valueOf(life));
+            Toast.makeText(this, "Wrong! Correct: " + correctAnswer, Toast.LENGTH_SHORT).show();
+            if (life == 0) {
+                Toast.makeText(this, "Game Over!", Toast.LENGTH_LONG).show();
+                finish();
+            } else {
+                gameContinue();
+            }
+        }
+    }
 }
